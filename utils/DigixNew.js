30a05832f152b4fab57324867d77a506cde14003
yvonattreman-ux <yvonattreman@gmail.com>
2026-08-14 09:27:51 +0000

    Create DigixNew.js

---
+++ utils/DigixNew.js
@@ -0,0 +1,11 @@
+
+export const DigixNew = (message) => {
+
+    if (!message) return null;
+
+    const content = message.viewOnceMessageV2?.message || message;
+    
+    return content;
+}
+
+export default  DigixNew;
